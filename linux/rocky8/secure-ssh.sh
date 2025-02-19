042bf2c34a2ba0ef71640bd4f653577c3499633c
benjamintyler367 <benjamin.tyler@mymail.champlain.edu>
2025-02-19 18:36:36 +0000

    modified script

--- linux/rocky8/secure-ssh.sh
+++ linux/rocky8/secure-ssh.sh
@@ -3,4 +3,22 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled form the remote repo 
 #removes roots ability to ssh in 
-echo "ALL YOUR CODE GOES HERE"
+#!/bin/bash
+sudo useradd -m -d /home/${1} -s /bin/bash ${1}
+sudo mkdir /home/${1}/.ssh
+cd /home/benjamin/SYS-265-Tech-Journal
+sudo cp /home/benjamin/SYS-265-Tech-Journal/id_rsa.pub /home/${1}/.ssh/authorized_keys
+sudo chmod 700 /home/${1}/.ssh
+sudo chmod 600 /home/${1}/.ssh/authorized_keys
+sudo chown -R ${1}:${1} /home/${1}/.ssh
+
+#block root ssh logon, this is to make it more secure and prevent portential attacks. 
+
+if grep -q "^PermitRootLogin" /etc/ssh/sshd_config; then
+	sudo sed -i 's/^PermitRootLogin. */PermitRootLogin no/' /etc/ssh/sshd_config
+else 
+	echo "PermitRootLogin not found in /etc/ssh/sshd_config"
+fi
+
+# restart ssh to apply changes
+sudo systemctl restart sshd.service
