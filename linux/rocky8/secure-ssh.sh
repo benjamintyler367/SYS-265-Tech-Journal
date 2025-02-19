8f480ba3d66f56977ad9dcdec80e7e41c33265a8
benjamintyler367 <benjamin.tyler@mymail.champlain.edu>
2025-02-18 21:44:31 -0500

    added what is needed for del 6

---
+++ linux/rocky8/secure-ssh.sh
@@ -0,0 +1,8 @@
+#secure-ssh.sh
+#author benjamintyler367
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled form the remote repo 
+#removes roots ability to ssh in 
+#!/bin/bash
+
+#
