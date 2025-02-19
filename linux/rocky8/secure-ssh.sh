8f7ce746e05a811af79f1e010051456da5f40574
benjamintyler367 <143431874+benjamintyler367@users.noreply.github.com>
2025-02-18 23:05:34 -0500

    Update secure-ssh.sh

--- linux/rocky8/secure-ssh.sh
+++ linux/rocky8/secure-ssh.sh
@@ -3,6 +3,4 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled form the remote repo 
 #removes roots ability to ssh in 
-#!/bin/bash
-
-#
+echo "ALL YOUR CODE GOES HERE"
